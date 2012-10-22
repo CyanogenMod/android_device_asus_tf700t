f978db1ec2932a907a22bc6dd13ffc010d299d02
thunderger <markushoeber@googlemail.com>
2012-10-23 00:19:20 +0200

    tf700: some cleanup
    Change-Id: I1f343ee850317565ea6cc553da689936e4b8e19e

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -115,10 +115,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.epad.model=TF700T \
     ro.product.model=TF700T
 
-# media files
+# Media configuration
 PRODUCT_COPY_FILES += \
-    device/asus/tf700t/media_codecs.xml:system/etc/media_codecs.xml \
-    device/asus/tf700t/media_profiles.xml:system/etc/media_profiles.xml
+    device/asus/tf700t/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    device/asus/tf700t/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # GPS configuration
 PRODUCT_COPY_FILES += \
