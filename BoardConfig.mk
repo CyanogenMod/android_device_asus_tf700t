9590f5bca3adc9b86902b25753cd62d40efae78a
Aaron Echols <atechols@gmail.com>
2012-10-25 18:01:57 -0700

    tf700t: disable tinyhal target
    Change-Id: I6d01120e9d1eb4b7fe267665f20ef9a9f91bd788

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@
 USE_PROPRIETARY_AUDIO_EXTENSIONS := true
 BOARD_USES_GENERIC_AUDIO := false
 BOARD_USES_ALSA_AUDIO := false
-BOARD_USES_TINY_AUDIO_HW := true
+BOARD_USES_TINY_AUDIO_HW := false 
 
 # inherit from the proprietary version
 -include vendor/asus/tf700t/BoardConfigVendor.mk
