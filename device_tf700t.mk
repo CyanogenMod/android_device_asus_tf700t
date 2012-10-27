64dfe1757e95980b652ff7874edcd58e530ed5ed
Aaron Echols <atechols@gmail.com>
2012-10-27 14:37:07 -0700

    tf700t: remove duplicate conf file
    cpu.sh is set in init.cardhu.cpu.rc, making this a redundant script,
    which was not being called.
    
    Change-Id: I7dbc1dad723509b237ad95a499710caa47649846

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilt configuration files
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/cpu.sh:system/bin/cpu.sh \
     $(LOCAL_PATH)/prebuilt/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/prebuilt/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
     $(LOCAL_PATH)/prebuilt/audio_policy.conf:system/etc/audio_policy.conf
