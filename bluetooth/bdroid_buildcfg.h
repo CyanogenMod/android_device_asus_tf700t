d8d87622acc49394396b8fadb020e04693161fa6
Aaron Echols <atechols@gmail.com>
2013-02-18 16:04:31 -0700

    tf700t: tethering and adapter fixes
    * Fixed bluetooth tethering
    * Fixed USB tethering
    * Fixed ethernet adapters detecting correctly
    
    Change-Id: I959d9ab13d1b47673547dff412edc788f409b884

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,13 +19,4 @@
 
 #define BTM_DEF_LOCAL_NAME "ASUS Transformer Pad TF700T"
 
-// Networking, Capturing, Object Transfer
-// MAJOR CLASS: COMPUTER
-// MINOR CLASS: PALM SIZE PC/PDA
-#define BTA_DM_COD {0x1A, 0x01, 0x14}
-
-#define BTIF_HF_SERVICES (BTA_HSP_SERVICE_MASK)
-#define BTIF_HF_SERVICE_NAMES  { BTIF_HSAG_SERVICE_NAME }
-#define PAN_NAP_DISABLED TRUE
-
 #endif
