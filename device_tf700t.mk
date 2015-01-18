64cfb39504cb1b2fa7ae63566aba4b131bfe5791
Jorge Ruesga <jorge@ruesga.com>
2015-01-18 02:22:07 +0100

    tf700t: return to 2048 dalvik heap
    Change-Id: Ic19c3f1377d9be4a1b758d26b44c2741853da85b
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -128,7 +128,7 @@ PRODUCT_COPY_FILES += \
     device/asus/tf700t/configs/gps.conf:system/etc/gps.conf
 
 # Inherit tablet dalvik settings
-$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
 # Broadcom wpa supplicant
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
